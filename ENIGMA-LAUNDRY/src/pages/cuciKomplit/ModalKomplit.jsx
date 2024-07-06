a3c908e44e5e2d3f9db27e97561e6b703c2d0f77
abi fathan yusuf <abiefathan333@gmail.com>
2024-07-06 16:24:50 +0700

    [note] push

--- ENIGMA-LAUNDRY/src/pages/cuciKomplit/ModalKomplit.jsx
+++ ENIGMA-LAUNDRY/src/pages/cuciKomplit/ModalKomplit.jsx
@@ -6,10 +6,10 @@ import { axiosinstance } from '../../lib/axios';
 
 const ModalKomplit = ({ isOpen, onClose }) => {
 
-  const [komplit,setkomplit]=useState({})
+  const [komplit, setkomplit] = useState({})
 
 
-const token =  localStorage.getItem("token")
+  const token = localStorage.getItem("token")
 
   const form = useForm({
     defaultValues: {
@@ -21,77 +21,80 @@ const token =  localStorage.getItem("token")
   })
 
 
-  const resultSubmit = async(data)=>{
-    try { const result = await axiosinstance.post("/products",{
-      headers : {Authorization:`Bearer ${token}`}
-    })
-      
+  const resultSubmit = async (data) => {
+    try {
+      const result = await axiosinstance.post("/products", data,
+        { headers: { Authorization: `Bearer ${token}` } }
+      )
+       console.log(result)
     } catch (error) {
-      
+     console.log(error)
     }
   }
 
 
 
   return (
-    <Modal isOpen={isOpen} onOpenChange={onClose}>
-      <ModalContent>
-        {(onClose) => (
-          <>
-            <ModalHeader className="flex flex-col gap-1 bg-slate-700 text-white" >Transaksi Baru</ModalHeader>
+    <div>
+      <Modal isOpen={isOpen} onOpenChange={onClose}>
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1 bg-slate-700 text-white" >Transaksi Baru</ModalHeader>
 
-            <ModalBody>
-              <form onSubmit={form.handleSubmit(resultSubmit)}>
-                <Controller
-                  name='name'
-                  control={form.control}
-                  render={({ field }) => {
-                    return (
-                      <Input {...field}
-                        label="JENIS PAKET"
-                        variant="bordered"
-                      />
-                    )
-                  }}
-                />
-                <Controller
-                  name='price'
-                  control={form.control}
-                  render={({ field }) => {
-                    return (
-                      <Input {...field}
-                        label="HARGA"
-                        variant="bordered"
-                        type='number'
-                      />
-                    )
-                  }}
-                />
-                <Controller
-                  name='type'
-                  control={form.control}
-                  render={({ field }) => {
-                    return (
-                      <Input {...field}
-                        label="BERAT"
-                        variant="bordered"
-                        type='number'
-                      />
-                    )
-                  }}
-                />
-                <Button color="danger" variant="flat" onPress={onClose}>
-                  Cancel
-                </Button>
-                <Button type='submit' color="primary" >
-                  Tambah Orderan
-                </Button >
-              </form>
-            </ModalBody>
-          </>
-        )}
-      </ModalContent>
-    </Modal>
+              <ModalBody>
+                <form onSubmit={form.handleSubmit(resultSubmit)}>
+                  <Controller
+                    name='name'
+                    control={form.control}
+                    render={({ field }) => {
+                      return (
+                        <Input {...field}
+                          label="JENIS PAKET"
+                          variant="bordered"
+                        />
+                      )
+                    }}
+                  />
+                  <Controller
+                    name='price'
+                    control={form.control}
+                    render={({ field }) => {
+                      return (
+                        <Input {...field}
+                          label="HARGA"
+                          variant="bordered"
+                          type='number'
+                        />
+                      )
+                    }}
+                  />
+                  <Controller
+                    name='type'
+                    control={form.control}
+                    render={({ field }) => {
+                      return (
+                        <Input {...field}
+                          label="BERAT"
+                          variant="bordered"
+                          type='number'
+                        />
+                      )
+                    }}
+                  />
+                  <Button color="danger" variant="flat" onPress={onClose}>
+                    Cancel
+                  </Button>
+                  <Button type='submit' color="primary" >
+                    Tambah Orderan
+                  </Button >
+                </form>
+              </ModalBody>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
   );
 };
 
